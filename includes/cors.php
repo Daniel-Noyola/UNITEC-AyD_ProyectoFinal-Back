202c8c032ec3ad7c4aba19df06867c16e531c4bd
Daniel Noyola <pepenoyola13@gmail.com>
2025-07-21 13:44:46 -0600

    Cors configurado

--- includes/cors.php
+++ includes/cors.php
@@ -1,5 +1,20 @@
-<?php 
-$origin = $_ENV['CORS_ORIGIN_URL'] ?? '';
-header("Access-Control-Allow-Origin: $origin");
+<?php
+$envOrigins = $_ENV['CORS_ALLOWED_ORIGINS'] ?? '';
+$allowedOrigins = array_map('trim', explode(',', $envOrigins));
+
+// Detectar origen de la solicitud
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
+    header("Vary: Origin");
+}
+
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-API-KEY");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}

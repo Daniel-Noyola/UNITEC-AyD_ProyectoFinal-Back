ed9a80be56c6fc7b8144be47a9d36c87d7457771
Daniel Noyola <pepenoyola13@gmail.com>
2025-07-22 11:11:11 -0600

    fixing

--- includes/cors.php
+++ includes/cors.php
@@ -8,7 +8,6 @@ $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 if (in_array($origin, $allowedOrigins)) {
     header("Access-Control-Allow-Origin: $origin");
     header("Access-Control-Allow-Credentials: true");
-    header("Vary: Origin");
 }
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");

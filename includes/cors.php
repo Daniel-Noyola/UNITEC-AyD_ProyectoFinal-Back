2c129fca9e8298663dbfab322c329e602af15cda
Daniel Noyola <pepenoyola13@gmail.com>
2025-07-24 10:20:18 -0600

    t

--- includes/cors.php
+++ includes/cors.php
@@ -10,7 +10,7 @@ if (in_array($origin, $allowedOrigins)) {
 }
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
